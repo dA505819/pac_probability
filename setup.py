d59fe2b0ada5dbb589ebd50e57806b4456ea2efc
Dhruv <dhruvaggarwal6@gmail.com>
2020-06-14 01:15:03 +0530

    Add files

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='pac_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['pac_probability'],
+      zip_safe=False)
